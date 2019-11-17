695e80309eab2aa13db7b13fb567ac3b6bdff15a
Delyan Angelov <delian66@gmail.com>
2019-11-17 22:26:24 +0200

    remove warnings about map...{} with latest v.

--- server/context.v
+++ server/context.v
@@ -32,7 +32,7 @@ pub mut:
 
 // Request
 pub fn (req Request) parse_form_body() map[string]string {
-    mut form_data_map := map[string]string{}
+    mut form_data_map := map[string]string
 
     if 'Content-Type' in req.headers && req.headers['Content-Type'] == 'application/x-www-form-urlencoded' {
         form_arr := req.body.split('&')
