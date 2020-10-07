91470344f156280786d5aaf6daf9cf49a2e91dee
Ned Palacios <npdoesmc@gmail.com>
2020-10-07 14:20:40 +0800

    fixed mime lib

--- mime/mime.v
+++ mime/mime.v
@@ -2,75 +2,69 @@ module mime
 
 import os
 
-
 pub struct MimeType {
 pub:
-    source string
-    extensions []string
-    compressible bool [skip]
-    charset string [skip]
+	source       string
+	extensions   []string
+	compressible bool     [skip]
+	charset      string   [skip]
 }
 
 struct Db {
 pub:
-    db map[string]MimeType
+	db map[string]MimeType
 }
 
 fn is_mime(text string) bool {
-    txt := text.split('/')
-
-    if txt.len != 2 { return false }
-
-    return true
+	txt := text.split('/')
+	if txt.len != 2 {
+		return false
+	}
+	return true
 }
 
 pub fn load() Db {
-    return Db{load_data()}
+	return Db{load_data()}
 }
 
 pub fn (mdb Db) charset(text string) string {
-    if !is_mime(text) && mdb.db[text.to_lower()].charset.len == 0 {
-        return ''
-    }
-
-    return mdb.db[text.to_lower()].charset
+	if !is_mime(text) && mdb.db[text.to_lower()].charset.len == 0 {
+		return ''
+	}
+	return mdb.db[text.to_lower()].charset
 }
 
 pub fn (mdb Db) content_type(text string) string {
-    mime := if !is_mime(text) { mdb.lookup(text) } else { text }
-
-    if mdb.db[mime].charset.len != 0 {
-        chrst := mdb.charset(mime)
-        
-        if chrst.len != 0 { 
-            return mime + '; charset=${chrst.to_lower()}'
-        }
-    }
-
-    return mime
+	mime := if !is_mime(text) { mdb.lookup(text) } else { text }
+	if mdb.db[mime].charset.len != 0 {
+		chrst := mdb.charset(mime)
+		if chrst.len != 0 {
+			return mime + '; charset=$chrst.to_lower()'
+		}
+	}
+	return mime
 }
 
 pub fn (mdb Db) extension(text string) string {
-    typ := mdb.db[text.to_lower()]
-
-    if !is_mime(text) || typ.extensions.len == 0 { return '' }
-
-    return typ.extensions[0]
+	typ := mdb.db[text.to_lower()]
+	if !is_mime(text) || typ.extensions.len == 0 {
+		return ''
+	}
+	return typ.extensions[0]
 }
 
 pub fn (mdb Db) lookup(path string) string {
-    path_ext := os.file_ext('x.${path}').to_lower()
-    extension := path_ext[1..path_ext.len]
-
-    if extension.len == 0 { return '' }
-
-    for k, v in mdb.db {
-        for x in v.extensions {
-            if x == extension {
-                return k
-            }
-        }
-    }
-
-    return ''
+	path_ext := os.file_ext('x.$path').to_lower()
+	extension := path_ext[1..path_ext.len]
+	if extension.len == 0 {
+		return ''
+	}
+	for k, v in mdb.db {
+		for x in v.extensions {
+			if x == extension {
+				return k
+			}
+		}
+	}
+	return ''
 }
