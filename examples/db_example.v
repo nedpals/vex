dbfeaaf85100b2e2e6d4c27d4ec84ef322db0657
Ned Palacios <npdoesmc@gmail.com>
2021-01-14 12:01:12 +0800

    examples: update db_example.v

--- examples/db_example.v
+++ examples/db_example.v
@@ -1,17 +1,18 @@
 module main
 
+import router
 import server
 import html
 import ctx
 import sqlite
 
-fn layout(body []html.Tag) html.Tag {
+fn layout(title string, body []html.Tag) html.Tag {
     template := html.Tag{name: 'html', children: [
         html.Tag{name: 'head', children: [
-            html.Tag{name: 'meta', props: { 'http-equiv': 'Content-Type', 'content': 'text/html;charset=UTF-8' }},
-            html.Tag{name: 'meta', props: { 'name': 'referrer', 'content': 'origin-when-cross-origin'}},
-            html.Tag{name: 'title', text: 'Vex SQLite Test'},
-            html.Tag{name: 'style', text: '
+            html.meta({ 'http-equiv': 'Content-Type', 'content': 'text/html;charset=UTF-8' }),
+            html.meta({ 'name': 'referrer', 'content': 'origin-when-cross-origin'}),
+            html.tag(name: 'title', text: '$title | Vex SQLite Test'),
+            html.style('
                 body {
                     width: 36rem;
                     margin: 0 auto;
@@ -20,38 +21,41 @@ fn layout(body []html.Tag) html.Tag {
                                     Cambria, Tahoma, Verdana, Arial, sans-serif;
                     color: #0C3C26;
                 }
-            '}
+            ')
         ]},
-        html.Tag{name: 'body', children: body}
+        html.block({name: 'body'}, body)
     ]}
 
     return template
 }
 
 fn main() {
-	db := sqlite.connect(':memory:')
-	mut s := server.new()
-	// db.exec('drop table if exists users;')
+	mut app := router.new()
+	db := sqlite.connect(':memory:')?
+	db.exec('drop table if exists users;')
 	db.exec('create table users (id integer primary key, name text default "");')
-	s.route(.get, '/', fn (req &ctx.Req, mut res ctx.Resp) {
+    app.inject(db)
+
+	app.route(.get, '/', fn (req &ctx.Req, mut res ctx.Resp) {
 		page := layout([
-            html.Tag{name: 'h1', text: 'It works!'},
-            html.Tag{name: 'p', children: [
-                html.Tag{name: 'text', text: 'For online documentation please refer to '},
-                html.Tag{name: 'a', props: { 'href': 'https://github.com/nedpals/vex' }, text: 'vex'},
-                html.Tag{name: 'br'},
-                html.Tag{name: 'em', text: 'It\'s a web framework based on ', children: [
-                    html.Tag{name: 'a', props: { 'href': 'https://vlang.io' }, text: 'V.'}
-                ]}
-            ]},
-        html.Tag{name: 'a', props: {'href': '/users'}, text: 'All users'},
-        html.Tag{name: 'br'},
-        html.Tag{name: 'a', props: {'href': '/users/add'}, text: 'Add a user'}
+            html.tag(name: 'h1', text: 'It works!'),
+            html.block({name: 'p'}, [
+                html.tag(name: 'text', text: 'For online documentation please refer to '),
+                html.tag(name: 'a', attr: { 'href': 'https://github.com/nedpals/vex' }, text: 'vex'),
+                html.br(),
+                html.block({name: 'em', text: 'It\'s a web framework based on '}, [
+                    html.Tag{name: 'a', attr: { 'href': 'https://vlang.io' }, text: 'V.'}
+                ])
+            ]),
+            html.tag(name: 'a', attr: {'href': '/users'}, text: 'All users'),
+            html.br(),
+            html.tag(name: 'a', attr: {'href': '/users/add'}, text: 'Add a user')
         ])
-		res.send_html(page, 200)
+		res.send_html(page.html(), 200)
 	})
-	s.route(.get, '/users', fn (req &ctx.Req, mut res ctx.Resp) {
-		users_from_db, _ := db.exec('select * from users;')
+	app.route(.get, '/users', fn (req &ctx.Req, mut res ctx.Resp) {
+        db2 := &sqlite.DB(req.ctx)
+		users_from_db, _ := db2.exec('select * from users;')
 		mut users := []html.Tag{}
 		for row in users_from_db {
 			tag := html.Tag{
@@ -61,34 +65,29 @@ fn main() {
 			users << tag
 		}
         page := layout([
-            html.Tag{name: 'h1', text: 'Users'},
-            html.Tag{name: 'ul', children: users},
-        html.Tag{name: 'a', props: {'href': '/'}, text: 'Back to homepage'}
+            html.tag(name: 'h1', text: 'Users'),
+            html.block({name: 'ul'}, users),
+            html.tag(name: 'a', attr: {'href': '/'}, text: 'Back to homepage')
         ])
-		res.send_html(page, 200)
+		res.send_html(page.html(), 200)
 	})
-	s.route(.get, '/users/add', fn (req &ctx.Req, mut res ctx.Resp) {
+	app.route(.get, '/users/add', fn (req &ctx.Req, mut res ctx.Resp) {
 		page := layout([
-            html.Tag{name: 'a', props: { 'href': '/users' }, text: 'All users'},
-            html.Tag{name: 'h1', text: 'Add user'},
-            html.Tag{name: 'form', props: { 'id': 'form', 'method': 'post', 'onsubmit': 'set_action(\'form\', \'name\')'}, children: [
-                html.Tag{name: 'input', props: { 'id': 'name', 'name': 'name', 'value': ''}},
-                html.Tag{name: 'button', props: { 'type': 'submit' }, text: 'Add'}
-            ]},
-            html.Tag{name: 'script', text: '
-                function set_action(f, n) {
-                    var form = document.getElementById(f);
-                    var name = document.getElementById(n).value;
-                    form.action = "/users/new/" + name;
-                }
-            '}
+            html.tag(name: 'a', attr: { 'href': '/users' }, text: 'All users'),
+            html.tag(name: 'h1', text: 'Add user'),
+            html.block({name: 'form', attr: { 'id': 'form', 'method': 'post', 'action': '/users/new' }}, [
+                html.Tag{name: 'input', attr: { 'id': 'name', 'name': 'name', 'value': ''}},
+                html.Tag{name: 'button', attr: { 'type': 'submit' }, text: 'Add'}
+            ])
         ])
-		res.send_html(page, 200)
+		res.send_html(page.html(), 200)
 	})
-	s.route(.post, '/users/new/:name', fn (req &ctx.Req, mut res ctx.Resp) {
-		name := req.params['name']
-		db.exec('insert into users (name) values ("$name");')
-		res.redirect('/users')
+	app.route(.post, '/users/new', fn (req &ctx.Req, mut res ctx.Resp) {
+        db2 := &sqlite.DB(req.ctx)
+        form_data := req.parse_body() or { map[string]string{} }
+		name := form_data['name']
+		db2.exec('insert into users (name) values ("$name");')
+        res.permanent_redirect('/users')
 	})
-	s.serve(8080)
+	server.serve(app, 8080)
 }
