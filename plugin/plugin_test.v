4770b194c789244e87e775734929e4b5ae0e89d0
Sandro Martini <sandro.martini@gmail.com>
2022-02-24 16:31:36 +0100

    add a plugin test, to ensure generic plugins works as expected

--- plugin/plugin_test.v
+++ plugin/plugin_test.v
@@ -1,5 +1,32 @@
 module plugin
 
+import router
+
+fn empty_implementation(mut router router.Router, options voidptr) {
+	println('hello from ' + @FN + ' initialized')
+	// other code (routes added, etc) omitted from here, see other examples ...
+}
+
 fn test_plugin() {
-	// stub
+	mut app := router.new()
+
+	// add a sample plugin using the generic factory function
+	mut generic_plugin := plugin.new('generic-plugin', '0.0.0', empty_implementation)
+	assert typeof(generic_plugin).name == '&plugin.Plugin'
+
+	// test plugin properties
+	assert generic_plugin.name() == 'generic-plugin'
+
+	// test plugin info
+	plugin_info_method := generic_plugin.info()
+	assert plugin_info_method.str() == "{'name': 'generic-plugin', 'version': '0.0.0', 'impl': 'set'}"
+
+	// test plugin info by calling its utility function
+	plugin_info_fn := plugin.plugin_info(generic_plugin)
+	assert plugin_info_fn.str() == plugin_info_method.str()
+
+	// register the plugin
+	// but don't call it (no need to start a server here)
+	app.register(generic_plugin, voidptr(0))
+	assert true
 }
