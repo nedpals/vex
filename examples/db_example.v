ae71ef169c44b6a8ecca7af56c97d785c48ac322
Ned <7358345+nedpals@users.noreply.github.com>
2022-03-22 10:13:12 +0800

    router, ctx: use `context` package for accessing app-specific contexts (#50)
    * router, ctx: use context package for setting/accessing contexts
    
    * ctx, router: use context for setting error status code
    
    * examples: update db_example.v + fmt

--- examples/db_example.v
+++ examples/db_example.v
@@ -5,22 +5,23 @@ import nedpals.vex.server
 import nedpals.vex.html
 import nedpals.vex.ctx
 import sqlite
+import context
 
 fn layout(title string, body []html.Tag) html.Tag {
 	template := html.html([
 		html.block(
 			name: 'head'
-		, children: [
-			html.meta({
-				'http-equiv': 'Content-Type'
-				'content':    'text/html;charset=UTF-8'
-			}),
-			html.meta({
-				'name':    'referrer'
-				'content': 'origin-when-cross-origin'
-			}),
-			html.tag(name: 'title', text: '$title | Vex SQLite Test'),
-			html.style('
+			children: [
+				html.meta({
+					'http-equiv': 'Content-Type'
+					'content':    'text/html;charset=UTF-8'
+				}),
+				html.meta({
+					'name':    'referrer'
+					'content': 'origin-when-cross-origin'
+				}),
+				html.tag(name: 'title', text: '$title | Vex SQLite Test'),
+				html.style('
                 body {
                     width: 36rem;
                     margin: 0 auto;
@@ -30,12 +31,24 @@ fn layout(title string, body []html.Tag) html.Tag {
                     color: #0C3C26;
                 }
             '),
-		]),
+			]
+		),
 		html.block(
 			name: 'body'
-		, children: body),
+			children: body
+		),
 	])
 	return template
+}
+
+const db_ctx_key = 'db'
+
+fn get_db(req &ctx.Req) ?&sqlite.DB {
+	raw_db := req.ctx.value(db_ctx_key) ?
+	if raw_db is sqlite.DB {
+		return raw_db
+	}
+	return none
 }
 
 fn main() {
@@ -43,35 +56,38 @@ fn main() {
 	db := sqlite.connect(':memory:') ?
 	db.exec('drop table if exists users;')
 	db.exec('create table users (id integer primary key, name text default "");')
-	app.inject(db)
+	app_ctx := context.with_value(context.todo(), db_ctx_key, db)
+	app.inject_context(app_ctx)
 	app.route(.get, '/', fn (req &ctx.Req, mut res ctx.Resp) {
 		page := layout('', [
 			html.tag(name: 'h1', text: 'It works!'),
 			html.block(
 				name: 'p'
-			, children: [
-				html.tag(name: 'text', text: 'For online documentation please refer to '),
-				html.tag(
-					name: 'a'
-					attr: {
-						'href': 'https://github.com/nedpals/vex'
-					}
-					text: 'vex'
-				),
-				html.br(),
-				html.block(
-					name: 'em'
-				, children: [
-					html.tag(name: 'text', text: "It's a web framework based on "),
+				children: [
+					html.tag(name: 'text', text: 'For online documentation please refer to '),
 					html.tag(
 						name: 'a'
 						attr: {
-							'href': 'https://vlang.io'
+							'href': 'https://github.com/nedpals/vex'
 						}
-						text: 'V.'
+						text: 'vex'
 					),
-				]),
-			]),
+					html.br(),
+					html.block(
+						name: 'em'
+						children: [
+							html.tag(name: 'text', text: "It's a web framework based on "),
+							html.tag(
+								name: 'a'
+								attr: {
+									'href': 'https://vlang.io'
+								}
+								text: 'V.'
+							),
+						]
+					),
+				]
+			),
 			html.tag(
 				name: 'a'
 				attr: {
@@ -91,7 +107,10 @@ fn main() {
 		res.send_html(page.html(), 200)
 	})
 	app.route(.get, '/users', fn (req &ctx.Req, mut res ctx.Resp) {
-		db2 := &sqlite.DB(req.ctx)
+		db2 := get_db(req) or {
+			res.send_status(500)
+			return
+		}
 		users_from_db, _ := db2.exec('select * from users;')
 		mut users := []html.Tag{}
 		for row in users_from_db {
@@ -105,7 +124,8 @@ fn main() {
 			html.tag(name: 'h1', text: 'Users'),
 			html.block(
 				name: 'ul'
-			, children: users),
+				children: users
+			),
 			html.tag(
 				name: 'a'
 				attr: {
@@ -133,28 +153,33 @@ fn main() {
 					'method': 'post'
 					'action': '/users/new'
 				}
-			, children: [
-				html.Tag{
-					name: 'input'
-					attr: {
-						'id':    'name'
-						'name':  'name'
-						'value': ''
-					}
-				},
-				html.Tag{
-					name: 'button'
-					attr: {
-						'type': 'submit'
-					}
-					text: 'Add'
-				},
-			]),
+				children: [
+					html.Tag{
+						name: 'input'
+						attr: {
+							'id':    'name'
+							'name':  'name'
+							'value': ''
+						}
+					},
+					html.Tag{
+						name: 'button'
+						attr: {
+							'type': 'submit'
+						}
+						text: 'Add'
+					},
+				]
+			),
 		])
 		res.send_html(page.html(), 200)
 	})
 	app.route(.post, '/users/new', fn (req &ctx.Req, mut res ctx.Resp) {
-		db2 := &sqlite.DB(req.ctx)
+		db2 := get_db(req) or {
+			eprintln(res)
+			res.send_status(500)
+			return
+		}
 		form_data := req.parse_form() or {
 			map[string]string{}
 		}
